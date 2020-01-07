29f290837d67e849e74788ed7e2044cf6286d133
git-leon <xleonhunter@gmail.com>
2020-01-07 09:51:47 -0500

    implemented `ItemInventory`

--- maven.productmanager/src/main/java/com/github/curriculeon/ItemInventory.java
+++ maven.productmanager/src/main/java/com/github/curriculeon/ItemInventory.java
@@ -1,31 +1,45 @@
 package com.github.curriculeon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 12/16/2019.
  */
 public class ItemInventory implements InventoryInterface<Item> {
+    private List<Item> list;
+    public ItemInventory(List<Item> prePopulatedList) {
+        this.list = prePopulatedList;
+    }
+
+    public ItemInventory() {
+        this.list = new ArrayList<>();
+    }
+
     @Override
     public void add(Item someObject) {
-
+        list.add(someObject);
     }
 
     @Override
     public Boolean contains(Item someObject) {
-        return null;
+        return list.contains(someObject);
     }
 
     @Override
     public Item get(int indexOfElement) {
-        return null;
+        return list.get(indexOfElement);
     }
 
     @Override
     public Item remove(int indexOfElement) {
-        return null;
+        Item result = list.get(indexOfElement);
+        list.remove(result);
+        return result;
     }
 
     @Override
     public Item[] toArray(Item[] objectsToBeAdded) {
-        return new Item[0];
+        return list.toArray(objectsToBeAdded);
     }
 }
